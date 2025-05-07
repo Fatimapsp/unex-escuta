ee90843079094ec7cc832fe75796ede583219d69
Rebeca Helen <rebecabatista165@gmail.com>
2025-05-07 03:17:54 -0300

    Estrutura inicial das rotas

---
+++ backend/routers/feedback.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+router.get('/feedback', (req, res) => {
+
+});
+
+router.get('/feedback/:id', (req, res) => {
+
+});
+
+router.get('/feedback/filter', (req, res) => {
+
+});
+
+router.post('/feedback', (req, res) => {
+    
+});
+
+router.delete('/feedback/:id', (req, res) => {
+    
+});
