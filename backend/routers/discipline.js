4f6b65cf901a712920472ce8ab9cc4f1089be2eb
Bia Lima <128002730+biaslima@users.noreply.github.com>
2025-05-12 16:01:09 -0300

    Rotas de disciplinas e professores

--- backend/routers/discipline.js
+++ backend/routers/discipline.js
@@ -1,27 +1,87 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const Discipline = require("../models/discipline");
+const { check, validationResult } = require("express-validator");
 
-router.get('/discipline', (req, res) => {
-
+//Busca todas as disciplinas
+router.get("/", async (req, res) => {
+  try {
+    const disciplines = await Discipline.find().populate();
+    res.status(200).json(disciplines);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.get('/discipline/:id', (req, res) => {
-
+//Busca todas disciplina por id
+router.get("/:id", async (req, res) => {
+  try {
+    const discipline = await Discipline.findById(req.params.id).populate(
+      "professors"
+    );
+    if (!discipline)
+      return res.status(404).json({ error: "Disciplina não encontrada" });
+    res.status(200).json(discipline);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.get('/discipline/name/:name', (req, res) => {
-
+//Buscar por nome
+router.get("/name/:name", async (req, res) => {
+  try {
+    const discipline = await Discipline.findOne({
+      name: { $regex: req.params.name, $options: "i" },
+    }).populate("professors");
+    if (!discipline)
+      return res.status(404).json({ error: "Disciplina não encontrada" });
+    res.status(200).json(discipline);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.post('/discipline', (req, res) => {
-    
+//Criar Disciplina
+router.post(
+  "/",
+  [
+    check("name", "O nome é obrigatório").notEmpty(),
+    check("code", "O código é obrigatório").notEmpty(),
+    check("courses", "Cursos deve ser uma lista").isArray(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const newDiscipline = new Discipline(req.body);
+      const saved = await newDiscipline.save();
+      res.status(200).json(saved);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
+
+//Atualizar disciplina
+router.put("/:id", async (req, res) => {
+  try {
+    const update = await Discipline.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-router.put('/discipline/:id', (req, res) => {
-    
-});
-
-router.delete('/discipline/:id', (req, res) => {
-    
+//Deletar disciplina
+router.delete("/:id", async (req, res) => {
+  try {
+    await Discipline.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Disciplina deletada com sucesso" });
+  } catch (err) {
+    res.status(400).json({ error: error.message });
+  }
 });
--- backend/routers/professor.js
+++ backend/routers/professor.js
@@ -1,27 +1,83 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const Professor = require("../models/professor");
+const { check, validationResult } = require("express-validator");
 
-router.get('/professor', (req, res) => {
-
+//Buscar todos os professores
+router.get("/", async (req, res) => {
+  try {
+    const professors = await Professor.find().populate();
+    res.status(200).json(professors);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.get('/professor/:id', (req, res) => {
-
+//Buscar professor por id
+router.get("/:id", async (req, res) => {
+  try {
+    const professor = await Professor.findById(req.params.id);
+    if (!professor)
+      return res.status(404).json({ error: "Professor não encontrado" });
+    res.status(200).json(professor);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.get('/professor/name/:name', (req, res) => {
-
+//Buscar professor por nome
+router.get("/name/:name", async (req, res) => {
+  try {
+    const professor = await Professor.findOne({
+      name: { $regex: req.params.name, $options: "i" },
+    });
+    if (!professor)
+      return res.status(404).json({ error: "Professor não encontrado" });
+    res.status(200).json(professor);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.post('/professor', (req, res) => {
-    
+//Criar Professor
+router.post(
+  "/",
+  [
+    check("name", "Nome é obrigatório").notEmpty(),
+    check("courses", "Cursos deve ser um array").isArray(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const newProfessor = new Professor(req.body);
+      const saved = await newProfessor.save();
+      res.status(200).json(saved);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+);
+
+//Atualizar professor
+router.put("/:id", async (req, res) => {
+  try {
+    const update = await Professor.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(update);
+  } catch (err) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-router.put('/professor/:id', (req, res) => {
-    
-});
-
-router.delete('/professor/:id', (req, res) => {
-    
+router.delete("/:id", async (req, res) => {
+  try {
+    await Professor.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Professor deletado com sucesso" });
+  } catch (err) {
+    res.status(400).json({ error: error.message });
+  }
 });
